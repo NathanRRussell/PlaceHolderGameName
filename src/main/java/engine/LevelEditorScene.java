3c3634c70f2d4f2eb325655c6c03bd960dfbaa2e
Nathan Russell <russellnathanielr@gmail.com>
2021-10-24 18:33:04 -0400

    finishing shader setup. Please no more

--- src/main/java/engine/LevelEditorScene.java
+++ src/main/java/engine/LevelEditorScene.java
@@ -1,8 +1,14 @@
 package engine;
 
+import org.lwjgl.BufferUtils;
+
 import java.awt.event.KeyEvent;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 
 import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
 public class LevelEditorScene extends Scene {
 
@@ -29,6 +35,22 @@ public class LevelEditorScene extends Scene {
             "}";
 
     private int vertexID, fragmentID, shaderProgram;
+
+    private float[] vertexArray = {
+             //position                 //color
+             0.5f, -0.5f, 0.0f,         1.0f, 0.0f, 0.0f, 1.0f, //Bottom right
+            -0.5f,  0.5f, 0.0f,         0.0f, 1.0f, 0.0f, 1.0f, //Top Left
+             0.5f,  0.5f, 0.0f,         0.0f, 0.0f, 1.0f, 1.0f, //Top Right
+            -0.5f, -0.5f, 0.0f,         1.0f, 1.0f, 0.0f, 1.0f, //Bottom Left
+    };
+
+    //Must be Counter-clockwise
+    private int[] elementArray = {
+            2, 1, 0, //Top Right
+            0, 1, 3  //Bottom Left
+    };
+
+    private int vaoID, vboID, eboID;
 
     public LevelEditorScene() {
 
@@ -79,10 +101,58 @@ public class LevelEditorScene extends Scene {
             System.out.println(glGetProgramInfoLog(shaderProgram, len));
             assert false : "";
         }
+
+        //Generate VAO, VBO, EBO
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        //Create float buffer
+        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
+        vertexBuffer.put(vertexArray).flip();
+
+        //Create VBO
+        vboID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
+
+        //Create indices
+        IntBuffer elementBuffer = BufferUtils.createIntBuffer(elementArray.length);
+        elementBuffer.put(elementArray).flip();
+
+        eboID = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+
+        //Add vertex attributes
+        int positionSize = 3;
+        int colorSize = 4;
+        int floatSizeBytes = 4;
+        int vertexSizeBytes = (positionSize + colorSize) * floatSizeBytes;
+        glVertexAttribPointer(0, positionSize, GL_FLOAT, false, vertexSizeBytes, 0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionSize * floatSizeBytes);
+        glEnableVertexAttribArray(1);
     }
 
     @Override
     public void update(float dt) {
+        //Bind Shader Program
+        glUseProgram(shaderProgram);
+        //Bind VAO
+        glBindVertexArray(vaoID);
 
+        //Enable Vertex Attributes
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+
+        glDrawElements(GL_TRIANGLES, elementArray.length, GL_UNSIGNED_INT, 0);
+
+        //Unbind
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+
+        glBindVertexArray(0);
+        glUseProgram(0);
     }
 }
